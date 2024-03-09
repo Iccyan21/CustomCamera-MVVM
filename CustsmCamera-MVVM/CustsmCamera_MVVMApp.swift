8732b5e2abadcd6941bc092dc48b9b039e2342ec
Iccyan21 <iccyan0429@gmail.com>
2024-03-09 14:18:33 +0900

    first commit

---
+++ CustsmCamera-MVVM/CustsmCamera_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustsmCamera_MVVMApp.swift
+//  CustsmCamera-MVVM
+//
+//  Created by 水原　樹 on 2024/03/09.
+//
+
+import SwiftUI
+
+@main
+struct CustsmCamera_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
